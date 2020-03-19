80c03b630d1ff1af29d55041bdce3bafbe87d8e3
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2020-03-19 20:04:05 +0200

    connect HTML add new event form to the TS code

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icons.component.css']
 })
 export class IconsComponent implements OnInit {
+  name: string = '';
+  civils_num: Number=-1;
+  eventSum_val: string = '';
+  soldiers_num: Number=-1;
+  part_num: Number=-1;
+  osn_val: string = '';
+  hour_val: string = '';
+  date_val: string = '';
+  description_val: string = '';
+  landmark_val: string = '';
+battalion_val: string = '';
+area_val: string = '';
+brigade_val: string = '';
+selected_event_type: string = '';
 
-  constructor() { }
+  constructor() {
+
+  }
+
 
   ngOnInit() {
   }
 
+ 
+
+  subAddForm(){
+  //   civils_num
+  //   eventSum_val
+  //   soldiers_num
+  //   part_num
+  //   osn_val
+  //   hour_val
+  //   date_val
+  //   description_val
+  //   landmark_val
+  // battalion_val
+  // area_val
+  // brigade_val
+  // selected_event_type
+    console.log(this.civils_num, this.eventSum_val, this.soldiers_num, this.part_num, this.osn_val, this.hour_val, this.date_val, this.description_val, this.landmark_val, this.battalion_val, this.area_val, this.brigade_val, this.selected_event_type);
+
+  }
 }
