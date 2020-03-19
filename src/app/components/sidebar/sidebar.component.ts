b6172848ab23436573bc1ecdeb9b92d670ea8317
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2020-03-19 14:31:35 +0200

    Return add event into user profile.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,11 +7,11 @@ declare interface RouteInfo {
     icon: string;
     class: string;
 }
+// the side nav page to edit
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'Add event',  icon:'person', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
     { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
 ];
