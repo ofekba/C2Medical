12efb109e04acbd62207dca52f0d9fc9847e6aa0
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2020-03-19 20:31:25 +0200

    connect the upgrade form to the ts code

--- src/app/upgrade/upgrade.component.ts
+++ src/app/upgrade/upgrade.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpgradeComponent implements OnInit {
 
+Caregiver_crew_val: string = '';
+Evacuation_crew_val: string = '';
+Evacuation_target_val: string = '';
+Injury_state_val: string = '';
+Urgent_val: string = '';
+riding_val: string = '';
+def_val: string = '';
+routine_val: string = '';
+Event_outline_val: string = '';
+Injury_mechanism_val: string = '';
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  subUpdateForm(){
+  
+      console.log(this.Caregiver_crew_val, this.Evacuation_crew_val, this.Evacuation_target_val, this.Injury_state_val, this.Urgent_val, this.riding_val, this.def_val, this.routine_val, this.Event_outline_val, this.Injury_mechanism_val);
+  
+    }
 
 }
