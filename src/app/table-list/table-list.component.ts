04db839bf949784753de4b936986724e309b86bf
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2020-03-19 22:56:39 +0200

    GET ALL EVENTS WORKING OFEKI IS THE QUEEN

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-table-list',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableListComponent implements OnInit {
 
-  constructor() { }
+  // let ItemsArray;
+  ItemsArray: any[] = [];
+  
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get(" http://localhost:3000/events").subscribe(
+      val => {
+          console.log("EVENT TABLE: GET call successful value returned in body", 
+                      val, Object.keys(val).length);
+                      for (let i = 0; i < Object.keys(val).length; i++) 
+                        this.ItemsArray[i]=val[i];
+                      console.log(this.ItemsArray);
+          
+      },
+      response => {
+          console.log("EVENT TABLE: GET call in error", response);
+      },
+      () => {
+          console.log("EVENT TABLE:The GET observable is now completed.");
+
+      }
+    );
   }
 
 }
