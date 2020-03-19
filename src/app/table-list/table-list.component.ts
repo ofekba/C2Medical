dbfa75161b06abb377f6138cbfeb3831c8dcfb77
Tomer Lev <levtom@post.bgu.ac.il>
2020-03-19 23:27:14 +0200

    Fix

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,12 +1,8 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  HttpClient
-} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
+  selector: 'app-table-list',
   templateUrl: './table-list.component.html',
   styleUrls: ['./table-list.component.css']
 })
@@ -14,36 +10,32 @@ export class TableListComponent implements OnInit {
 
   // let ItemsArray;
   ItemsArray: any[] = [];
+  
 
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get(" http://localhost:3000/events").subscribe(
       val => {
-        console.log("EVENT TABLE: GET call successful value returned in body",
-          val, Object.keys(val).length);
-        for (let i = 0; i < Object.keys(val).length; i++)
-          this.ItemsArray[i] = val[i];
-        console.log(this.ItemsArray);
-
+          console.log("EVENT TABLE: GET call successful value returned in body", 
+                      val, Object.keys(val).length);
+                      for (let i = 0; i < Object.keys(val).length; i++) 
+                        this.ItemsArray[i]=val[i];
+                      console.log(this.ItemsArray);
+          
       },
       response => {
-        console.log("EVENT TABLE: GET call in error", response);
+          console.log("EVENT TABLE: GET call in error", response);
       },
       () => {
-        console.log("EVENT TABLE:The GET observable is now completed.");
+          console.log("EVENT TABLE:The GET observable is now completed.");
 
       }
     );
   }
 
-<<<<<<< HEAD
-}
-=======
   handleEvent(id){
     console.log("lallaala", id);
   }
 
 }
->>>>>>> 59ab6fec5a64a7f5f0c5230af918f4057b9b2b72
