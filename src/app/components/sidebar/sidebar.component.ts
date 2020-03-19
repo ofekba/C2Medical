13080aed4506fc5e8a3c7944ba7bac38fe86b88f
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2020-03-19 14:12:48 +0200

    ADD NEW EVENT FORM

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,7 +9,7 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/user-profile', title: 'Add event',  icon:'person', class: '' },
     { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
     { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
