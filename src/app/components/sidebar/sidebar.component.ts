c3333e808f982b3c1ee297702abb82bd2c405d8d
Ofek Barazani <37291940+ofekba@users.noreply.github.com>
2020-03-20 09:10:20 +0200

    Fixes.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -13,8 +13,8 @@ export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/table-list', title: 'Pending Events Table',  icon:'update', class: '' },
     { path: '/upgrade', title: 'Handled Events Table',  icon:'content_paste', class: '' },
-    { path: '/icons', title: 'Add New Event',  icon:'note_add', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/icons', title: 'Add New Event',  icon:'note_add', class: '' }
+ //   { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
     // { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
     // { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
   ];
